27803b8509da26215d38933f06acd78f388dc495
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-05-06 20:24:00 -0500

    Verify projects are connected

--- Library/Library.fs
+++ Library/Library.fs
@@ -3,3 +3,4 @@
 module Say =
     let hello name =
         printfn "Hello %s" name
+        sprintf "Ohayo %s san" name
--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -1,8 +1,15 @@
 module Tests
 
 open System
+open FsUnit.Xunit
 open Xunit
+
+open Library.Say
 
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
+
+[<Fact>]
+let ``Libary Smoke Test`` () =
+    hello "Tanaka" |> should equal "Ohayo Tanaka san"
