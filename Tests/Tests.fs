794488420237a8ac8b185abb1d8a6125157ab6cc
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-05-14 17:08:06 -0500

    tests: Add tests for createMessageUrl

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -13,6 +13,13 @@ let ``can create NPC URL from type and year`` () =
     createNpcPageUrl ALNAV 20 |> should equal "http://www.public.navy.mil/bupers-npc/reference/messages/ALNAVS/Pages/ALNAV2020.aspx"
 
 [<Fact>]
+let ``can create message URL fragment`` () =
+    createMessageUrl NAVADMIN 15 42 |> should equal "bupers-npc/reference/messages/Documents2/NAV2015/NAV15042.txt"
+    createMessageUrl NAVADMIN 20 3 |> should equal "bupers-npc/reference/messages/Documents/NAVADMINS/NAV2020/NAV20003.txt"
+    createMessageUrl ALNAV 15 9 |> should equal "bupers-npc/reference/messages/Documents/ALNAVS/ALN2015/ALN15009.txt"
+    createMessageUrl ALNAV 20 123 |> should equal "bupers-npc/reference/messages/Documents/ALNAVS/ALN2020/ALN20123.txt"
+
+[<Fact>]
 let ``can get current year in YY format`` () =
     getCurrentYear() |> should equal 20
 
