0a058403e066a463722cf5f932ab1fb2d9947d86
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-05-17 14:56:44 -0500

    fix: Update scratchpad to support FParsec module

--- scratchpad.fsx
+++ scratchpad.fsx
@@ -1,6 +1,13 @@
 
-#r "./Library/bin/Debug/netstandard2.0/Library.dll"
+#I @"packages\FParsec\lib\net45"
+#r @"FParsecCS.dll"
+#r @"FParsec.dll"
+#r @"Library\bin\Debug\netstandard2.0\Library.dll"
 
 open NavySearch.Common
+open NavySearch.Message
+open NavySearch.Message.Parser
 
 ["foo"; "bar"] |> List.reduce join
+
+"     " |> removeExtraSpaces
