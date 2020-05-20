9a2f6dbe57818bb0b293a204b68c8ef7f6119057
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-05-20 09:59:33 -0500

    dev: Start trying to add AltCover to build

--- build.fsx
+++ build.fsx
@@ -1,37 +1,38 @@
 #r "paket:
 nuget Fake.IO.FileSystem
 nuget Fake.DotNet.Cli
-nuget Fake.Core.Target //"
+nuget Fake.Core.Target
+nuget AltCover.Fake //"
 #load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
+open AltCover_Fake.DotNet.Testing
 
 Target.create "install" (fun _ ->
-  Trace.log " --- Installing dependencies --- "
-  DotNet.exec id "paket" "install" |> ignore
-  DotNet.exec id "paket" "restore" |> ignore
-)
+    Trace.log " --- Installing dependencies --- "
+    DotNet.exec id "paket" "install" |> ignore
+    DotNet.exec id "paket" "restore" |> ignore)
 
 Target.create "build" (fun _ ->
-  Trace.log " --- Building files --- "
-  DotNet.build id ""
-)
+    Trace.log " --- Building files --- "
+    DotNet.build id "")
 
-Target.create "test" (fun _ -> 
-  Trace.log " --- Running Tests ---"
-  DotNet.test id "Tests"
-)
+Target.create "test" (fun _ ->
+    Trace.log " --- Running Tests ---"
+    let setBaseOptions (o: DotNet.Options) =
+        { o with
+              WorkingDirectory = Path.getFullName "./Tests"
+              Verbosity = Some DotNet.Verbosity.Minimal }
+
+    DotNet.test (fun o -> o.WithCommon(setBaseOptions)) "Tests.fsproj")
 
 Target.create "setup" (fun _ -> Trace.log " --- Project is ready --- ")
 
 open Fake.Core.TargetOperators
 
 // *** Define Dependencies ***
-"install"
-  ==> "build"
-  ==> "test"
-  ==> "setup"
+"install" ==> "build" ==> "test" ==> "setup"
 
 Target.runOrDefault "setup"
