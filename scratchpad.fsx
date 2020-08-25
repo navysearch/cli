a951a0705b0a9c251460c80ea030ed961656ba54
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-08-24 20:26:37 -0500

    fix: Make scratchpad work on Linux, remove FSharp.Data import

--- scratchpad.fsx
+++ scratchpad.fsx
@@ -1,15 +1,12 @@
-#time
-#I @"packages\Algolia.Search\lib\netstandard2.0"
-#I @"packages\FParsec\lib\net45"
-#I @"packages\Fsharp.Data\lib\net45"
-#I @"packages\dotenv.net\lib\netstandard2.0"
-#I @"packages\Newtonsoft.Json\lib\netstandard2.0"
+#I @"packages/Algolia.Search/lib/netstandard2.0"
+#I @"packages/FParsec/lib/net45"
+#I @"packages/dotenv.net/lib/netstandard2.0"
+#I @"packages/Newtonsoft.Json/lib/netstandard2.0"
 #r "Algolia.Search.dll"
 #r "FParsecCS.dll"
 #r "FParsec.dll"
-#r "FSharp.Data.dll"
 #r "dotenv.net.dll"
-#r @"Library\bin\Debug\netstandard2.0\Library.dll"
+#r @"Library/bin/Debug/netstandard2.0/Library.dll"
 
 open System
 open System.Net
@@ -25,8 +22,9 @@ open NavySearch.Message.Parser
 // let id = getEnvVar "ALGOLIA_APP_ID"
 // let key = getEnvVar "ALGOLIA_ADMIN_API_KEY"
 
-let localPath = @"C:\Users\jason\dev\messages"
+// let localPath = @"C:\Users\jason\dev\messages"
+// let downloaded, failed = DownloadMessagesByYear NAVADMIN 20 localPath
+// downloaded
+// failed
 
-let downloaded, failed = DownloadMessagesByYear NAVADMIN 20 localPath
-downloaded
-failed
+let boot = 42
